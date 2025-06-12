dcf7cf5576c4da54023c838fe23b45f18a895232
avel <av568614@gmail.com>
2025-06-12 23:50:55 +0400

    PART COMMIT

--- run.sh
+++ run.sh
@@ -23,6 +23,7 @@ echo "Выберите действие с файлом $C_FILE_PATH..."
 echo "1) build"
 echo "2) up"
 echo "3) down"
+echo "4) build main file only"
 
 read -p "Введите соответствующий номер операции над файлом (1-3): " command
 
@@ -30,13 +31,17 @@ case $command in
   1) COMMAND="build" ;;
   2) COMMAND="up" ;;
   3) COMMAND="down" ;;
+  4) COMMAND="build main file only" ;;
   *) echo "Неверный номер операции"; exit 1 ;;
 esac
 
 if [ "$COMMAND" = "build" ]; then
-  docker-compose -f docker-compose.yml -f "$C_FILE_PATH" build
+  docker-compose -f docker-compose.yml -f "$C_FILE_PATH" build --no-cache
 elif [ "$COMMAND" = "up" ]; then
   docker-compose -f docker-compose.yml -f "$C_FILE_PATH" up
 elif [ "$COMMAND" = "down" ]; then
   docker-compose -f docker-compose.yml -f "$C_FILE_PATH" down
+#TODO: доработать (сейчас не работает)
+elif [ "$COMMAND" = "build main file only" ]; then
+   docker-compose -f docker-compose.yml -f "$C_FILE_PATH" exec api go build -o main cmd/main.go
 fi
