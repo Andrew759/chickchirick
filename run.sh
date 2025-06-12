9ca2707af029a7c40fb7d3ca557ed62f17de5361
avel <av568614@gmail.com>
2025-06-12 06:32:29 +0400

    Добавлена команда down в скрипт run.sh; Рефакторинг в правках по окружениям докера #2, в том числе правки ошибок

--- run.sh
+++ run.sh
@@ -22,17 +22,21 @@ esac
 echo "Выберите действие с файлом $C_FILE_PATH..."
 echo "1) build"
 echo "2) up"
+echo "3) down"
 
-read -p "Введите соответствующий номер операции над файлом (1-2): " command
+read -p "Введите соответствующий номер операции над файлом (1-3): " command
 
 case $command in
   1) COMMAND="build" ;;
   2) COMMAND="up" ;;
+  3) COMMAND="down" ;;
   *) echo "Неверный номер операции"; exit 1 ;;
 esac
 
 if [ "$COMMAND" = "build" ]; then
   docker-compose -f docker-compose.yml -f "$C_FILE_PATH" build
-elif [ "COMMAND" = "up" ]; then
+elif [ "$COMMAND" = "up" ]; then
   docker-compose -f docker-compose.yml -f "$C_FILE_PATH" up
+elif [ "$COMMAND" = "down" ]; then
+  docker-compose -f docker-compose.yml -f "$C_FILE_PATH" down
 fi
