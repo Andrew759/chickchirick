2c77027d17832b810346188e9ae6fe1287456aef
avel <av568614@gmail.com>
2025-07-02 17:58:58 +0400

    PART COMMIT

---
+++ pkg/chirik_faker/faker.go
@@ -0,0 +1,60 @@
+package chirik_faker
+
+import (
+	"chickChirick/pkg/chirik_migrator/db_schema"
+	"encoding/json"
+	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
+	"time"
+)
+
+func FakeValue(fieldName string, dataType db_schema.DataType) string {
+	switch fieldName {
+
+	}
+	switch dataType {
+	case db_schema.Bool:
+		return fmt.Sprintf("%v", gofakeit.Bool())
+
+	case db_schema.Smallint:
+		return fmt.Sprintf("%d", gofakeit.IntRange(-32768, 32767))
+
+	case db_schema.Int:
+		return fmt.Sprintf("%d", gofakeit.IntRange(-2147483648, 2147483647))
+
+	case db_schema.Bigint:
+		return fmt.Sprintf("%d", gofakeit.Int64())
+
+	case db_schema.Real:
+		return fmt.Sprintf("%.4f", gofakeit.Float32Range(-1000, 1000))
+
+	case db_schema.DoublePrecision:
+		return fmt.Sprintf("%.6f", gofakeit.Float64Range(-1e6, 1e6))
+
+	case db_schema.Varchar, db_schema.Text:
+		return fmt.Sprintf("'%s'", gofakeit.Sentence(5))
+
+	case db_schema.TimestampWithTimezone:
+		return fmt.Sprintf("'%s'", gofakeit.Date().Format(time.RFC3339))
+
+	case db_schema.TimestampWithoutTimezone:
+		return fmt.Sprintf("'%s'", gofakeit.Date().Format("2006-01-02 15:04:05"))
+
+	case db_schema.Uuid:
+		return fmt.Sprintf("'%s'", gofakeit.UUID())
+
+	case db_schema.Json, db_schema.Jsonb:
+		fakeMap := map[string]string{
+			"name":  gofakeit.FirstName(),
+			"email": gofakeit.Email(),
+		}
+		b, _ := json.Marshal(fakeMap)
+		return fmt.Sprintf("'%s'", string(b))
+
+	case db_schema.Null:
+		return "NULL"
+
+	default:
+		return "'unsupported_type'"
+	}
+}
