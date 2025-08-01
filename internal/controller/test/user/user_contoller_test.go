c2f355c698990d13c43e59ecf3934e96849483fa
avel <av568614@gmail.com>
2025-08-01 22:36:33 +0400

    PART COMMIT для автотестов

--- internal/controller/test/user/user_contoller_test.go
+++ internal/controller/test/user/user_contoller_test.go
@@ -2,7 +2,7 @@ package u_test
 
 import (
 	"bytes"
-	appCongig "chickChirick/cmd/config"
+	appConfig "chickChirick/cmd/config"
 	"chickChirick/cmd/factory"
 	"chickChirick/cmd/service"
 	"chickChirick/internal/controller/abstraction"
@@ -10,6 +10,7 @@ import (
 	"chickChirick/internal/middleware/config"
 	userMiddleware "chickChirick/internal/middleware/validators/user"
 	userModels "chickChirick/internal/model/user"
+	"chickChirick/pkg/chirik_gorm_tweaks"
 	"context"
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
@@ -18,79 +19,83 @@ import (
 	"testing"
 )
 
-//TODO: убедиться, что многократное открытие клиентов не привидет к ошибкам
-
-// TODO: удалить хардкод
-const ServerURL = "http://127.0.0.1:8081"
-
 type ExternalServices struct {
 	service.DBDecorator
 	service.RedisDecorator
 }
 
-type UCContainer struct {
+type UserControllerTestContainer struct {
+	ServerURL string
 	ExternalServices
 	HttpServer *httptest.Server
 	HttpClient *http.Client
 	*user.UserController
 }
 
-func initUCContainer(t *testing.T) UCContainer {
+func initUCContainer(t *testing.T) UserControllerTestContainer {
+	t.Helper()
 	factory.InitViper()
 
-	appConfig := appCongig.AppConfiguration{}.NewAppConfiguration()
+	appCfg := appConfig.AppConfiguration{}.NewAppConfiguration()
+	db := service.InitORM(&appCfg.DatabaseConfig)
+	redis := service.InitRedis(appCfg.RedisConfig)
 
-	dbDecorator := service.InitORM(&appConfig.DatabaseConfig)
-	defer dbDecorator.CloseDB()
+	createUserTable(db)
 
-	redisDecorator := service.InitRedis(appConfig.RedisConfig)
-	defer redisDecorator.RedisClose()
+	server := startTestServer(t, db, redis)
 
 	ac := abstraction.Controller{
 		Dependencies: abstraction.DIContainer{
-			DBDecorator: dbDecorator,
+			DBDecorator: db,
 		},
 	}
 	uv := userMiddleware.UserValidator{}
 
-	//создание таблицы пользователя
-	createUserTable(dbDecorator)
-
-	//старт тестового сервера
-	httpServer := startTestServer(t, dbDecorator, redisDecorator)
-
-	return UCContainer{
+	uCTC := UserControllerTestContainer{
+		ServerURL: appCfg.ServerURL,
 		ExternalServices: ExternalServices{
-			DBDecorator:    dbDecorator,
-			RedisDecorator: redisDecorator,
+			DBDecorator:    db,
+			RedisDecorator: redis,
 		},
-		HttpServer: httpServer,
+		HttpServer: server,
 		HttpClient: factory.InitHttpClient(),
 		UserController: &user.UserController{
 			Controller:    ac,
 			UserValidator: uv,
 		},
 	}
+
+	t.Cleanup(func() {
+		dropUserTable(uCTC)
+		uCTC.HttpServer.Close()
+		uCTC.DBDecorator.CloseDB()
+		uCTC.RedisDecorator.RedisClose()
+	})
+
+	return uCTC
 }
 
-func createUserTable(dbDecorator service.DBDecorator) {
-	err := dbDecorator.GDB().AutoMigrate(userModels.User{})
+func createUserTable(db service.DBDecorator) {
+	if err := db.GDB().AutoMigrate(&userModels.User{}); err != nil {
+		panic("failed to migrate user table: " + err.Error())
+	}
+}
+
+func dropUserTable(uCTC UserControllerTestContainer) {
+	tableName, err := chirik_gorm_tweaks.GetTableName(uCTC.GDB(), userModels.User{})
 	if err != nil {
-		panic(err)
+		panic("failed to get table name: " + err.Error())
+	}
+	_, err = uCTC.NativeDB().Exec("DROP TABLE IF EXISTS " + tableName + " CASCADE;")
+	if err != nil {
+		panic("failed to drop schema: " + err.Error())
 	}
 }
 
 func startTestServer(t *testing.T, db service.DBDecorator, redis service.RedisDecorator) *httptest.Server {
 	t.Helper()
-
 	mux := factory.BuildServer(db, redis)
-	server := httptest.NewServer(mux)
-
-	t.Cleanup(func() {
-		server.Close()
-	})
-
-	return server
+	return httptest.NewServer(mux)
 }
 
 func TestCreateSuccess(t *testing.T) {
@@ -102,54 +107,56 @@ func TestCreateSuccess(t *testing.T) {
 		Phone:   "+79634823344",
 		Login:   "andrey_velkov",
 	}
-	body, _ := json.Marshal(newUser)
+	body, err := json.Marshal(newUser)
+	assert.NoError(t, err)
 
-	req, _ := http.NewRequest(http.MethodPost, ServerURL+"/user", bytes.NewBuffer(body))
+	req, err := http.NewRequest(http.MethodPost, ucc.ServerURL+"/user", bytes.NewBuffer(body))
+	assert.NoError(t, err)
+
 	req.Header.Set("Content-Type", "application/json")
-
-	// middleware обходит контекст, эмуляция
 	ctx := context.WithValue(req.Context(), config.UserUserKey, &newUser)
 	req = req.WithContext(ctx)
 
 	resp, err := ucc.HttpClient.Do(req)
 	assert.NoError(t, err)
+	defer resp.Body.Close()
+
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 }
 
 func TestCreateAndGetSuccess(t *testing.T) {
 	ucc := initUCContainer(t)
 
-	newUser := userModels.User{
+	user := userModels.User{
 		Name:    "Andrey",
 		Surname: "Velkov",
 		Phone:   "+79634823344",
 		Login:   "andrey_velkov",
 	}
-	body, _ := json.Marshal(newUser)
+	body, err := json.Marshal(user)
+	assert.NoError(t, err)
 
-	req, _ := http.NewRequest(http.MethodPost, ServerURL+"/user", bytes.NewBuffer(body))
+	req, err := http.NewRequest(http.MethodPost, ucc.ServerURL+"/user", bytes.NewBuffer(body))
+	assert.NoError(t, err)
 	req.Header.Set("Content-Type", "application/json")
-
-	// middleware обходит контекст, эмуляция
-	ctx := context.WithValue(req.Context(), config.UserUserKey, &newUser)
+	ctx := context.WithValue(req.Context(), config.UserUserKey, &user)
 	req = req.WithContext(ctx)
 
 	resp, err := ucc.HttpClient.Do(req)
-
-	//TODO: обработать ошибку
+	assert.NoError(t, err)
 	defer resp.Body.Close()
-	var j map[string]any
-	err = json.NewDecoder(resp.Body).Decode(&j)
 
-	//Получение списка всех пользователей
-	//TODO: скорее всего не будет работать
-	resp, err = ucc.HttpClient.Get(ServerURL + "/user?id=" + j["id"].(string))
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 
-	var users []userModels.User
-	err = json.NewDecoder(resp.Body).Decode(&users)
-	assert.NoError(t, err)
-	assert.Len(t, users, 1)
-	assert.Equal(t, "Andrey", users[0].Name)
+	//resp, err = ucc.HttpClient.Get(ucc.ServerURL + "/user?id=" + id)
+	//assert.NoError(t, err)
+	//defer resp.Body.Close()
+	//
+	//assert.Equal(t, http.StatusOK, resp.StatusCode)
+	//
+	//var users []userModels.User
+	//err = json.NewDecoder(resp.Body).Decode(&users)
+	//assert.NoError(t, err)
+	//assert.Len(t, users, 1)
+	//assert.Equal(t, "Andrey", users[0].Name)
 }
